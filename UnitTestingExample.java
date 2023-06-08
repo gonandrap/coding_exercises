27abf66451b32a827e4c618235193af99dba8228
Gonzalo <gonzalo.a.r@gmail.com>
2023-06-08 10:36:17 -0700

    adding unit tests for java

---
+++ UnitTestingExample.java
@@ -0,0 +1,33 @@
+
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.*;
+import org.junit.runner.*;
+
+
+public class UnitTestingExample {
+    
+    public int sum(int a, int b) {
+        return a+b;
+    }
+
+    public int excp_trigger() throws NullPointerException {
+        System.out.println("Trowing an exception");
+        throw new NullPointerException();
+    }
+
+    @Test 
+    public void testSum() {
+        UnitTestingExample obj = new UnitTestingExample();
+        assertEquals(obj.sum(1,2), 3);
+        assertEquals(obj.sum(2,2), 4);
+        Assert.assertTrue(obj.sum(2,2) == 4);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testExcep() {
+        UnitTestingExample obj = new UnitTestingExample();
+        obj.excp_trigger();
+    }
+}
