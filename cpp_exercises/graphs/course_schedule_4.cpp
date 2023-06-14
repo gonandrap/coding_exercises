efb8010e6840dae8e86f63a99b28d0701c957b7b
Gonzalo <gonzalo.a.r@gmail.com>
2023-06-14 16:03:29 -0700

    finally finished the course schedule 4, the hardest so far!

--- cpp_exercises/graphs/course_schedule_4.cpp
+++ cpp_exercises/graphs/course_schedule_4.cpp
@@ -11,6 +11,13 @@ using namespace std;
 class CourseScheduleFour
 {
 public:
+ 
+    
+
+    /*
+        This solution is giving me time limit exceeded, which makes sense because I'm trying to build an enourmous graph, where for each node,
+        I have a set of all posible dependencies (direct + indirect). Even though that is correct, seems to be an overkill.
+    */
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries)
     {
         /*
@@ -24,9 +31,11 @@ public:
         map<int,set<int>> * neighbors = buildNeighbors(prerequisites, new map<int,set<int>>());         // step 1
 
         set<int> permMark;
-        set<int> tempMark;
         for (auto currentEdge : prerequisites)          // the goal of this iteration is the step 2 of the algorithm}
         {
+            //cout << "Iterating current edge [" << currentEdge[0] << "," << currentEdge[1] << "]" << endl;
+
+            set<int> tempMark;
             int currentNode = currentEdge[0];
 
             set<int>* currentIterationDeps;
@@ -44,9 +53,7 @@ public:
 
             if (permMark.find(currentNode) == permMark.end())
             {
-                tempMark.insert(currentNode);       // subgraph with root=currentNode starting to be processed, mark the root temporarly
-                iterateSubGraph(currentNode, neighbors, tempMark, permMark, currentIterationDeps, indDeps);
-                tempMark.erase(currentNode);
+                iterateSubGraph(currentNode, neighbors, permMark, indDeps);
                 permMark.insert(currentNode);       // subgraph with root=currentNode was processed, can be marked permanently
             }
         }
@@ -63,53 +70,90 @@ public:
     }
 
 private:
-    void iterateSubGraph(int currentNode, const map<int,set<int>> * globalNeighbors, set<int> & tempMark, const set<int> & permMark, set<int>* currentIterationDeps, map<int, set<int>* > & globalIndDeps)
+    void iterateSubGraph(int currentNode, const map<int,set<int>> * globalNeighbors, set<int> & permMark, map<int, set<int>* > & globalIndDeps)
     {
+        //cout << "Iteration subgraph with root=" << currentNode << endl;
+
+        set<int> * tempMark = new set<int>();               // tempMark = dependencies (direct + indirect) of subgraph with root=currentNode
         auto currentNeighbors_it = globalNeighbors->find(currentNode);
         if (currentNeighbors_it != globalNeighbors->end())
         {
             auto currentNeighbors = currentNeighbors_it->second;
             for (auto currentNeighbor : currentNeighbors)
             {
+                tempMark->insert(currentNeighbor);      // every direct neighbor is a direct dependency for "currentNode"
+
                 if (permMark.find(currentNeighbor) != permMark.end())
                 {
-                    currentIterationDeps->insert(currentNeighbor);      // first add the current neighbor as dependency
-
                     /*
                         since all ind dependencies were already identified for this current neighbor, we 
                         dont  need to iterate again, but we do have to add all of its deps
                         to the current iteration
                     */ 
-                    auto currentNeighborDeps_it = globalIndDeps.find(currentNeighbor);
-                    if (currentNeighborDeps_it != globalIndDeps.end())      // not necessary, but it's work to have the extra check
-                    {
-                        copy(currentNeighborDeps_it->second->begin(), currentNeighborDeps_it->second->end(), inserter(*currentIterationDeps, currentIterationDeps->begin()));
-                    } else
-                    {
-                        cout << "node [" << currentNeighbor << "] not found in [globalIndDeps]" << endl;
-                    }
-                    
-                    
                 } else
                 {
-                    // need to recursively iterate over the subgraph to detect all the indirect dependencies
-                    if (tempMark.find(currentNeighbor) != tempMark.end())
-                    {
-                        /*
-                            I dont need to do anything special. I still need to go through it because there are other siblings that still need to be identified
-                            QUESTION : do I really need the "tempMark" then?
-                        */
-                    }
-                    currentIterationDeps->insert(currentNeighbor);
-                    tempMark.insert(currentNeighbor);
-                    iterateSubGraph(currentNeighbor, globalNeighbors, tempMark, permMark, currentIterationDeps, globalIndDeps);
+                    iterateSubGraph(currentNeighbor, globalNeighbors, permMark, globalIndDeps);
                 }
+                keepIndirectDeps(*tempMark, currentNeighbor, globalIndDeps);
             }
+            /*
+                When I'm done with all the neighbors (and their neighbors, recursively), "tempMark" has all nodes visited, which represents the indirectDependencies,
+                which tells me that "currentNeighbor" can be flag with the "permMark"
+            */
+            //cout << "Done iteration of neighbors for [" << currentNode << "]" << endl;
+            permMark.insert(currentNode);
+            addIndirectDeps(*tempMark, currentNode, globalIndDeps);
+            return;
         } else 
         {
             /*
                 currentNode doesn't have any neighbors, so no need of keep iterating
             */
+            //cout << "No neighbors for [" << currentNode << "]" << endl;
+
+            //NOTE : let's try not adding the leafs to as "permMark"
+            //permMark.insert(currentNode);
+            addIndirectDeps(*tempMark, currentNode, globalIndDeps);
+            return;
+        }
+
+        // Once we are done iterating, we can add the dependencies identified in "tempMark" to globalIndDeps[currentNode]
+        //cout << "Done iteration of neighbors for [" << currentNode << "]" << endl;
+        permMark.insert(currentNode);
+        addIndirectDeps(*tempMark, currentNode, globalIndDeps);
+    }
+
+
+    /*
+        Will copy from "destinyDependencies[destinyKey] to "pendingDependencies"
+    */
+    void keepIndirectDeps(set<int> & pendingDependencies, int destinyKey, map<int, set<int>* > & destinyDependencies)
+    {
+        auto destinyDependencies_it = destinyDependencies.find(destinyKey);
+        if (destinyDependencies_it != destinyDependencies.end())      // not necessary, but it's work to have the extra check
+        {
+            copy(destinyDependencies_it->second->begin(), destinyDependencies_it->second->end(), inserter(pendingDependencies, pendingDependencies.begin()));
+        } else 
+        {
+            cout << "[keepIndirectDeps] couldnt find dependencies for destinyKey=" << destinyKey << endl;
+        }
+    }
+
+    /*
+        Will copy from "pendingDependencies" to "destinyDependencies[destinyKey]"
+    */
+    void addIndirectDeps(set<int> & pendingDependencies, int destinyKey, map<int, set<int>* > & destinyDependencies)
+    {
+        auto destinyDependencies_it = destinyDependencies.find(destinyKey);
+        if (destinyDependencies_it != destinyDependencies.end())      // not necessary, but it's work to have the extra check
+        {
+            copy(pendingDependencies.begin(), pendingDependencies.end(), inserter(*destinyDependencies_it->second, destinyDependencies_it->second->begin()));
+        } else
+        {
+            // we first need to create the entry in "destinyDependencies" for "destinyKey"
+            set<int> * destinyDependenciesContainer = new set<int>();
+            copy(pendingDependencies.begin(), pendingDependencies.end(), inserter(*destinyDependenciesContainer, destinyDependenciesContainer->begin()));
+            destinyDependencies.insert({destinyKey, destinyDependenciesContainer});
         }
     }
 
@@ -140,10 +184,10 @@ int main(int argc, char* argv[])
 {
     CourseScheduleFour sol = CourseScheduleFour();
     int numCourses = 5;
-    vector<vector<int>> preReqs = {{3,4},{2,3},{1,2},{0,1}};
+    vector<vector<int>> preReqs = {{0,1},{1,2},{2,3},{3,4}};
     vector<vector<int>> queries = {{0,4},{4,0},{1,3},{3,0}};
-    cout << "result=";
     auto result = sol.checkIfPrerequisite(numCourses, preReqs, queries);
+    cout << "result=";
     copy(result.cbegin(), result.cend(), ostream_iterator<int>(cout, " "));
     cout << endl;
 }
