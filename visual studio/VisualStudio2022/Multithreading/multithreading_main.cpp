1c94ba4a64a3daed005a6860874419847d2f650b
gonandrap <gonzalo.a.r@gmail.com>
2023-07-23 15:19:52 -0700

    simplest example of multithreading using modern C++

---
+++ visual studio/VisualStudio2022/Multithreading/multithreading_main.cpp
@@ -0,0 +1,46 @@
+// Multithreading.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <sstream>
+
+std::mutex stdout_mutex;
+
+void print_stdout(std::string msg)
+{
+    std::lock_guard<std::mutex> guard(stdout_mutex);
+    std::cout << msg << std::endl;
+}
+
+void sum(int a, int b)
+{
+    size_t i = 0;
+    while (i++ < 10)
+    {
+        std::stringstream ss;
+        ss << "sum of numbers" << a << " and " << b;
+        print_stdout(ss.str());       
+    }
+}
+
+int main()
+{
+    std::cout << "Starting" << std::endl;
+
+    std::thread t1(sum, 1, 2);
+    std::thread t2(sum, 2, 6);
+    std::thread t3(sum, 1, 9);
+    std::thread t4(sum, 4, 1);
+    std::thread t5(sum, 9, 22);
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+
+    std::cout << "Finished" << std::endl;
+}
+
